86c2c87d157b42a9d6b8fe49829246e67d3f58f9
alessandroscagnetti <alessandro.scagnetti@studenti.unicam.it>
2025-02-25 22:14:02 +0100

    Delete src/main/java/it/cs/unicam/ids/filiera/domainModel/Observer.java

--- src/main/java/it/cs/unicam/ids/filiera/domainModel/Observer.java
+++
@@ -1,9 +0,0 @@
-package it.cs.unicam.ids.filiera.domainModel;
-
-public interface Observer {
-
-	/**
-	 * Method that permit Observer to update the subject
-	 */
-	public void update(Subject subject);
-}
