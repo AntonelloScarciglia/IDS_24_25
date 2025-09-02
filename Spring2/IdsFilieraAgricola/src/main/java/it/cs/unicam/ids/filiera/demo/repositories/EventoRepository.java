0c5bd5745d9181d2c8ca222becb452b020cf5210
AntonelloScarciglia <antonello.scarciglia@studenti.unicam.it>
2025-09-02 19:41:15 +0200

    ignora
    ignora

--- Spring2/IdsFilieraAgricola/src/main/java/it/cs/unicam/ids/filiera/demo/repositories/EventoRepository.java
+++
@@ -1,8 +0,0 @@
-package it.cs.unicam.ids.filiera.demo.repositories;
-
-import it.cs.unicam.ids.filiera.demo.entity.eventi.Evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventoRepository extends JpaRepository<Evento, Long> {
-
-}
