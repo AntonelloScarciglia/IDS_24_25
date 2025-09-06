eb589e078ec5dc414c8e3ebc4ce367b2fdd1079e
Seya <146985506+Seya26@users.noreply.github.com>
2025-09-06 18:48:44 +0200

    Update CuratoreObserver.java

--- Spring/IdsFilieraAgricola/src/main/java/it/cs/unicam/ids/filiera/demo/observer/CuratoreObserver.java
+++ Spring/IdsFilieraAgricola/src/main/java/it/cs/unicam/ids/filiera/demo/observer/CuratoreObserver.java
@@ -2,8 +2,15 @@ package it.cs.unicam.ids.filiera.demo.observer;
 
 public class CuratoreObserver implements Observer{
 
+    private UtenteRepository utenteRepository;
+
     @Override
     public void aggiorna(Notifica notifica, String messaggio) {
-
+        if(notifica instanceof Prodotto prodotto){
+            for(UtenteVerificato u : utenteRepository.findAllCuratori()){
+                u.getNotifiche().add(messaggio);
+            }
+        }
     }
 }
+
