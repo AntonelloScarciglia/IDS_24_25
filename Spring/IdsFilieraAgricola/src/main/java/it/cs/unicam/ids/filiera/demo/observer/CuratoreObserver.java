f76fd522812b886c59ad0a30bd3ac592b8cdc7c6
Seya <146985506+Seya26@users.noreply.github.com>
2025-09-06 19:02:20 +0200

    Update CuratoreObserver.java

--- Spring/IdsFilieraAgricola/src/main/java/it/cs/unicam/ids/filiera/demo/observer/CuratoreObserver.java
+++ Spring/IdsFilieraAgricola/src/main/java/it/cs/unicam/ids/filiera/demo/observer/CuratoreObserver.java
@@ -1,5 +1,11 @@
 package it.cs.unicam.ids.filiera.demo.observer;
 
+import it.cs.unicam.ids.filiera.demo.entity.Prodotto;
+import it.cs.unicam.ids.filiera.demo.entity.UtenteVerificato;
+import it.cs.unicam.ids.filiera.demo.repositories.UtenteRepository;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CuratoreObserver implements Observer{
 
     private UtenteRepository utenteRepository;
@@ -13,4 +19,5 @@ public class CuratoreObserver implements Observer{
         }
     }
 }
+
 
